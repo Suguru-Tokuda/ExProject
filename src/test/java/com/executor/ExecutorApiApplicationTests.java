13af4511285330a579d9006c2789fc63722530a2
Suguru Tokuda <sugurut@wolfram.com>
2018-10-31 13:24:19 -0500

    Changed the folder name

--- src/test/java/com/executor/ExecutorApiApplicationTests.java
+++ src/test/java/com/executor/ExecutorApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.exProject.ExProjectAPI;
+package com.executor;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ExProjectApiApplicationTests {
+public class ExecutorApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
